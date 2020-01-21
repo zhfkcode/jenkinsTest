9bf9e1fe7dc018c330e330fcea95c7afdafb6292
zhcode <zhlearn@sina.com>
2020-01-21 15:54:19 +0800

    jjj

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
   </div>
 </template>
 
-<style lang="scss">
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,14 +19,14 @@
 
 #nav {
   padding: 30px;
-
-  a {
+}
+ #nav  a {
     font-weight: bold;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+ }
+  #nav .router-link-exact-active {
+    color: #42b983;
   }
-}
+
+
 </style>
